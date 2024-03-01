33985d986b180b23cd908e91d09f22cdaa8320f6
borroot <inbox@borroot.com>
2024-03-01 17:56:36 +0100

    Same thing, but now with json

--- src/plot.py
+++ src/plot.py
@@ -1,26 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import json
 from os import listdir
 
-folder = 'output/'
-files = sorted([f for f in listdir(folder) if not 'final' in f])
+out_folder = 'arrow_output/'
+in_folder = 'output/'
 
-for i in range(0, len(files), 2):
-    Xs = np.genfromtxt(folder + files[i + 1], delimiter=",")
-    img = plt.imread(folder + files[i])
+# load the json file containing the results
+with open(in_folder + 'results.json') as f:
+    results = json.load(f)
 
-    plt.figure(figsize=(7, 7))
+for experiment in results:
+    for run in experiment['runs']:
+        img = plt.imread(in_folder + run['img'])
 
-    plt.quiver(Xs[:, 0], 250 - Xs[:, 1], Xs[:, 2], -Xs[:, 3], color='red')
-    plt.scatter(Xs[:, 0], 250 - Xs[:, 1], color='red')
+        plt.figure(figsize=(7, 7))
 
-    plt.imshow(img, extent=(0, 250, 0, 250))
+        xy = np.array(run['xy'])
+        dxy = np.array(run['dxy'])
 
-    plt.xlim(0, 250)
-    plt.ylim(0, 250)
+        plt.quiver(xy[:, 0], 250 - xy[:, 1], dxy[:, 0], -dxy[:, 1], color='red')
+        plt.scatter(xy[:, 0], 250 - xy[:, 1], color='red')
 
-    plt.axis('off')
+        plt.imshow(img, extent=(0, 250, 0, 250))
 
-    plt.savefig(folder + 'final_' + files[i])
-    # plt.show()
+        plt.xlim(0, 250)
+        plt.ylim(0, 250)
+
+        plt.axis('off')
+
+        plt.savefig(out_folder + run['img'])
+        # plt.show()
